f325a98c4efa4990c607f42579444ca8f0d1603c
Novianto <novianto778@gmail.com>
2020-08-11 20:33:36 +0700

    header part commit

--- component/header/header.js
+++ component/header/header.js
@@ -1,7 +1,22 @@
 class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    
+    <header class="header-component">
+    <nav>
+      <div class="container">
+        <h1 class="header-logo" href="">Indonesia</h1>
+        <ul class="menu-container">
+          <li><a href="">Awesomes</a></li>
+          <li><a href="">Video</a></li>
+          <li><a href="">Story</a></li>
+          <li><a href="">About Us</a></li>
+        </ul>
+        <div class="hamburger-menu">
+          <i class="fa fa-bars fa-2x"></i>
+        </div>
+      </div>
+    </nav>
+  </header>
     `
   }
 }
